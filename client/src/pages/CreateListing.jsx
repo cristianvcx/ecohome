ef6b6c0feb77fbe0c0920f10409975c9807dc69d
Cristian Andres <cristianvcx188@gmail.com>
2024-05-13 23:33:31 -0400

    actualizar pagina listado UI

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -3,55 +3,115 @@ import React from 'react'
 export default function CreateListing() {
     return (
         <main className='p-3 max-w-4xl mx-auto'>
-            <h1 className='text-3xl font-semibold text-center my-7'>Crear un Listado</h1>
+            <h1 className='text-3xl font-semibold text-center my-7'>
+                Crear un Listado
+            </h1>
             <form className='flex flex-col sm:flex-row gap-4'>
                 <div className='flex flex-col gap-4 flex-1'>
-                    <input type="text" placeholder="Nombre" className="border p-3 
-                rounded-lg" id='name' maxLength='62' minLength='10' required />
-                    <textarea type="text" placeholder="Descripcion" className="border p-3 
-                rounded-lg" id='description' required />
-                    <input type="text" placeholder="Direccion" className="border p-3 
-                rounded-lg" id='address' required />
+                    <input
+                        type="text"
+                        placeholder="Nombre"
+                        className="border p-3 rounded-lg"
+                        id='name'
+                        maxLength='62'
+                        minLength='10'
+                        required
+                    />
+                    <textarea
+                        type="text"
+                        placeholder="Descripcion"
+                        className="border p-3 rounded-lg"
+                        id='description'
+                        required
+                    />
+                    <input
+                        type="text"
+                        placeholder="Direccion"
+                        className="border p-3 rounded-lg"
+                        id='address'
+                        required
+                    />
                     <div className="flex gap-6 flex-wrap">
                         <div className="flex gap-2">
-                            <input type="checkbox" id='sale' className='w-5' />
+                            <input
+                                type="checkbox"
+                                id='sale'
+                                className='w-5'
+                            />
                             <span>Vender</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='rent' className='w-5' />
+                            <input
+                                type="checkbox"
+                                id='rent'
+                                className='w-5'
+                            />
                             <span>Alquilar</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='parking' className='w-5' />
+                            <input
+                                type="checkbox"
+                                id='parking'
+                                className='w-5'
+                            />
                             <span>Estacionamiento</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='furnished' className='w-5' />
+                            <input
+                                type="checkbox"
+                                id='furnished'
+                                className='w-5'
+                            />
                             <span>Amueblado</span>
                         </div>
                         <div className="flex gap-2">
-                            <input type="checkbox" id='offer' className='w-5' />
+                            <input
+                                type="checkbox"
+                                id='offer'
+                                className='w-5'
+                            />
                             <span>Oferta</span>
                         </div>
                     </div>
                     <div className="flex flex-wrap gap-6">
-                        <div className="flex item-center gap-2">
-                            <input type="number" id="bedrooms" min='1' max='10' required className="p-3 border border-gray-300 rounded-lg" />
+                        <div className="flex items-center gap-2">
+                            <input
+                                type="number"
+                                id="bedrooms"
+                                min='1'
+                                max='10'
+                                required
+                                className="p-3 border border-gray-300 rounded-lg"
+                            />
                             <p>Camas</p>
                         </div>
-                        <div className="flex item-center gap-2">
-                            <input type="number" id="bathrooms" min='1' max='10' required className="p-3 border border-gray-300 rounded-lg" />
+                        <div className="flex items-center gap-2">
+                            <input
+                                type="number"
+                                id="bathrooms"
+                                min='1'
+                                max='10'
+                                required
+                                className="p-3 border border-gray-300 rounded-lg"
+                            />
                             <p>Baños</p>
                         </div>
-                        <div className="flex item-center gap-2">
-                            <input type="number" id="regularPrice" min='1' max='10' required className="p-3 border border-gray-300 rounded-lg" />
+                        <div className="flex items-center gap-2">
+                            <input
+                                type="number"
+                                id="regularPrice"
+                                min='1'
+                                max='10'
+                                required
+                                className="p-3 border border-gray-300 rounded-lg"
+                            />
                             <div className="flex flex-col items-center">
                                 <p>Precio Regular</p>
                                 <span className='text-xs'>($ / Mes)</span>
                             </div>
 
                         </div>
-                        <div className="flex item-center gap-2">
+                        <div className="flex items-center gap-2">
                             <input type="number" id="discountPrice" min='1' max='10' required className="p-3 border border-gray-300 rounded-lg" />
                             <div className="flex flex-col items-center">
                                 <p>Precio descontado</p>
@@ -61,10 +121,10 @@ export default function CreateListing() {
                     </div>
                 </div>
                 <div className='flex flex-col flex-1 gap-4'>
-                    <p className="font-semibold">Imagen: 
-                    <span className="font-normal text-gray-600 ml-2">La primera imagen será la portada (max 6)</span></p>
+                    <p className="font-semibold">Imagen:
+                        <span className="font-normal text-gray-600 ml-2">La primera imagen será la portada (max 6)</span></p>
                     <div className="flex gap-4">
-                        <input className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple/>
+                        <input className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple />
                         <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Actualizar</button>
                     </div>
                     <button className="p-3 bg-slate-700 text-white rounded-lg uppercarse hover:opacity-95 disabled:opacity-80">Crear Lista</button>
