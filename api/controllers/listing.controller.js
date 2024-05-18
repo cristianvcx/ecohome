9f526d2f21e71261b97765d030d33baaa3c9ec05
Cristian Andres <cristianvcx188@gmail.com>
2024-05-17 20:55:35 -0400

    Funcionalidad completa de eliminacion de listado de usuarios

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -7,4 +7,23 @@ export const createListing = async(req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
+
+export const deleteListing = async (req, res, next)=>{
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(404, 'Listado no encontrado!'));
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, '¡Solo puedes eliminar tus propios anuncios!'))
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('¡El listado ha sido eliminado!');
+    } catch (error) {
+        next(error);
+    }
+};
