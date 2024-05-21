82f27ed7a1101382364742b9369c3c29ce7da000
Cristian Andres <cristianvcx188@gmail.com>
2024-05-20 22:45:58 -0400

    agrege la funcionalidad de contacto de propietario a la pagina de listado

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
+import {useSelector} from 'react-redux';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
 import { FaBath, FaBed, FaChair, FaMapMarkedAlt, FaParking, FaShare } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 export default function Listing() {
     SwiperCore.use([Navigation]);
@@ -12,7 +14,10 @@ export default function Listing() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [copied, setCopied] = useState(false);
-    const params = useParams()
+    const [contact, setContact] = useState(false);
+    const params = useParams();
+    const {currentUser} = useSelector((state) => state.user);
+    
     useEffect(() => {
         const fetchListing = async () => {
             try {
@@ -34,7 +39,6 @@ export default function Listing() {
         }
         fetchListing()
     }, [params.listingId]);
-    console.log(loading);
     return (
         <main>
             {loading && <p className='text-center my-7 text-2xl'>Cargando...</p>}
@@ -95,22 +99,26 @@ export default function Listing() {
                     </p>
                     <ul className='text-green-900 font-semibold text-sm flex items-center gap-4 sm:gap-6'>
                         <li className='flex items-center gap-1 whitespace-nowrap'>
-                            <FaBed className='text-lg'/>
+                            <FaBed className='text-lg' />
                             {listing.bedrooms > 1 ? `${listing.bedrooms} camas ` : `${listing.bedrooms} camas `}
                         </li>
                         <li className='flex items-center gap-1 whitespace-nowrap'>
-                            <FaBath className='text-lg'/>
+                            <FaBath className='text-lg' />
                             {listing.bathrooms > 1 ? `${listing.bathrooms} baño ` : `${listing.bathrooms} baños `}
                         </li>
                         <li className='flex items-center gap-1 whitespace-nowrap'>
-                            <FaParking className='text-lg'/>
+                            <FaParking className='text-lg' />
                             {listing.parking ? 'Estacionamiento' : 'No Estacionamiento'}
                         </li>
                         <li className='flex items-center gap-1 whitespace-nowrap'>
-                            <FaChair className='text-lg'/>
+                            <FaChair className='text-lg' />
                             {listing.furnished ? 'Amueblado' : 'No Amueblado'}
                         </li>
                     </ul>
+                    {currentUser && listing.userRef !== currentUser._id && !contact && (
+                        <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>Contactar al Propietario</button>
+                    )}
+                    {contact && <Contact listing={listing}/>}
                 </div>
             </div>
             )}
